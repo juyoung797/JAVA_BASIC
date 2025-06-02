0af43f8f31fbfb6e544c7cd153e1b4270482ff06
juyoung <juyoungjeong.797@gmail.com>
2025-06-02 14:39:40 +0900

    if-else문 클래스 분리

---
+++ src/IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+    int num = 1;
+    public void function() {
+        if (num > 0) {
+            System.out.println("number is positive");
+        } else if (num < 0) {
+            System.out.println("number is negative");
+        } else {
+            System.out.println("number is zero");
+        }
+    }
+}
