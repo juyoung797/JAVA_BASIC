b76476642c917660320a0f1a2b5228b8ad089b55
juyoung <juyoungjeong.797@gmail.com>
2025-06-02 14:39:07 +0900

    switch case 문 추가

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,22 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int number = -1;
-        if (number > 0) {
-            System.out.println("number is positive");
-        } else if (number < 0) {
-            System.out.println("number is negative");
-        } else {
-            System.out.println("number is zero");
+        String mood = "happy";
+        switch (mood) {
+            case "happy":
+                System.out.println("나는 행복하다");
+                break;
+            case "angry":
+                System.out.println("나는 화가났다");
+                break;
+            case "sad":
+                System.out.println("나는 슬프다");
+                break;
+            case "fear":
+                System.out.println("나는 겁이난다");
+                break;
+            default:
+                System.out.println("나는 편안하다");
         }
     }
-}
-
-class Old {
-
 }
