00eaef914dab0ff0b6269bdbb5da0cff2b3ef0eb
juyoung <juyoungjeong.797@gmail.com>
2025-06-02 15:12:27 +0900

    minor changes

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String mood = "happy";
-        switch (mood) {
-            case "happy":
-                System.out.println("나는 행복하다");
-                break;
-            case "angry":
-                System.out.println("나는 화가났다");
-                break;
-            case "sad":
-                System.out.println("나는 슬프다");
-                break;
-            case "fear":
-                System.out.println("나는 겁이난다");
-                break;
-            default:
-                System.out.println("나는 편안하다");
-        }
+
     }
 }
