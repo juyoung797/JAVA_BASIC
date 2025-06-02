8d3849f2763fbbad686e03587add8f1fa6d28cc5
juyoung <juyoungjeong.797@gmail.com>
2025-06-02 13:42:51 +0900

    if else 문 추가

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int number = -1;
+        if (number > 0) {
+            System.out.println("number is positive");
+        } else if (number < 0) {
+            System.out.println("number is negative");
+        } else {
+            System.out.println("number is zero");
         }
     }
+}
+
+class Old {
+
 }
