03988168add460a34e0a7961af5efff6ebf2fe4c
juyoung <juyoungjeong.797@gmail.com>
2025-06-04 14:52:00 +0900

    Add While-Loop-Case

--- src/WhileLoop.java
+++ src/WhileLoop.java
@@ -2,6 +2,9 @@ public class WhileLoop {
     //초기값 세팅
     int i = 0;
     boolean a = false;
+    //키입력
+    int input = 8;
+    
     public void run() {
         while (i < 5) {
             System.out.println(i); //반복할 코드
@@ -18,5 +21,37 @@ public class WhileLoop {
         do {
             System.out.println("무한루프");
         } while (a);
+    }
+    public void whileCase() {
+        boolean a = true;
+        int b = 1;
+        while (a) {
+            System.out.println(b);
+            b++;
+
+            if (b > 100) {
+                a = false;
+            }
+        }
+    }
+    public void doWhileCase() {
+        do {
+            System.out.println("안녕하세요 자판기 입니다.");
+            //키입력
+            switch (input) {
+                case 1:
+                    System.out.println("콜라");
+                    break;
+                case 2:
+                    System.out.println("사이다");
+                    break;
+                case 3:
+                    System.out.println("물");
+                    break;
+                default:
+                    System.out.println("1, 2, 3 중에 입력하세요");
+            }
+            input = 0;
+        } while (input != 0);
     }
 }
