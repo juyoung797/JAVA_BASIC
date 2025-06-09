ecfec9242442e38b73ef04a79f5ae6345b775b0e
juyoung <juyoungjeong.797@gmail.com>
2025-06-09 13:20:39 +0900

    Add reverse, sum Array

---
+++ src/Arrays.java
@@ -0,0 +1,18 @@
+public class Arrays {
+    int[] numbers = {2, 4, 6, 8, 10};
+    public static void reverseArray(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[numbers.length - 1 - i]);
+        }
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            System.out.println(numbers[i]);
+        }
+    }
+    public static void sumArray(int[] numbers {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        System.out.println(sum);
+    }
+}
