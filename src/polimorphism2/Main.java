524ff5613f19568cb789aab25c1647785ac49b73
juyoung <juyoungjeong.797@gmail.com>
2025-06-13 18:07:56 +0900

    minor change

--- src/polimorphism2/Main.java
+++ src/polimorphism2/Main.java
@@ -3,22 +3,47 @@ package polimorphism2;
 abstract class Animal {
     abstract void makeSound();
     public void sleep() {
-        System.out.println("Sleeping...");
+        System.out.println("자는중...");
     }
 }
 class Dog extends Animal {
     @Override
     void makeSound() {
-        System.out.println("bark bark");
+        System.out.println("멍멍");
     }
 }
 interface flyable {
-    void fly(); //인터페이스는 단일 기능을
+    void fly();
+}
+interface layEggs {
+    void layEggs();
+}
+class Bird extends Animal implements flyable, layEggs {
+    @Override
+    void makeSound() {
+        System.out.println("짹짹");
+    }
+    @Override
+    public void fly() {
+        System.out.println("날기");
+    }
+    @Override
+    public void layEggs() {
+        System.out.println("알 낳기");
+    }
 }
 public class Main {
     public static void main(String[] args) {
         Animal dog = new Dog();
         dog.makeSound();
         dog.sleep();
+        Animal bird = new Bird();
+        bird.makeSound();
+        bird.sleep();
+        if (bird instanceof Bird) {
+            Bird bird2 = (Bird) bird;
+            bird2.fly();
+            bird2.layEggs();
+        }
     }
 }
