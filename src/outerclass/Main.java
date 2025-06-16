f8c4070ea7ab4364bf084a29aedfa48fa5badb61
juyoung <juyoungjeong.797@gmail.com>
2025-06-16 13:13:22 +0900

    outer private

---
+++ src/outerclass/Main.java
@@ -0,0 +1,34 @@
+package outerclass;
+class OuterClass {
+    String outerMessage = "This is from OuterClass";
+    private class InnerClass {
+        void printMessage() {
+            System.out.println(outerMessage);
+        }
+    }
+    InnerClass createInnerClass() {
+        return new InnerClass();
+    }
+}
+
+public class Main {
+    String outerMessage = "Hello form Outer";
+    class Inner {
+        void showMessage() {
+            System.out.println(outerMessage);
+        }
+    }
+    public static void main(String[] args) {
+        // 1. Outer instance 생성
+        Main main = new Main();
+        // 2. Inner instance 생성 (outer.new 사용)
+        Main.Inner inner = main.new Inner();
+        // 3. Inner 클래스 메서드 호출
+        inner.showMessage();
+
+        OuterClass outerClass = new OuterClass();
+//        OuterClass.InnerClass = Outer.new InnerClass();
+//        outerClass.createInnerClass().printMessage();
+
+    }
+}
