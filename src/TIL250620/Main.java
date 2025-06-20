360995e10de30a239934ec38bf5ab66e909a49f1
juyoung <juyoungjeong.797@gmail.com>
2025-06-20 09:39:52 +0900

    feat: Stream Even Sum

---
+++ src/TIL250620/Main.java
@@ -0,0 +1,15 @@
+package TIL250620;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        int result = numbers.stream()
+                .filter((n) -> n % 2 == 0)
+                .mapToInt(x -> x.intValue())
+                .sum(); // 2, 4
+        System.out.println(result);
+    }
+}
