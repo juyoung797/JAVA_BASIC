e6823bbf21852e2987dd23d71423fbe1d836cdc9
juyoung <juyoungjeong.797@gmail.com>
2025-06-23 14:31:41 +0900

    javadoc

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"TIL250623.annotation","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"TIL250623.annotation","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"TIL250623.annotation","l":"Main"}];updateSearchResults();
